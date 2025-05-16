65cb69dd5d91dbbf12eecaaa6cca5095278b3385
Ryan <ra.zhang@hl-it.cn>
2025-05-16 09:40:16 +0800

    test

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -33,9 +33,9 @@ solver: libmamba
 CONDARC
 #mv /opt/conda/conda-meta/history /opt/conda/conda-meta/history.$(date +%Y-%m-%d-%H-%M-%S)
 #echo > /opt/conda/conda-meta/history
-micromamba install --root-prefix ~/.conda --prefix /opt/conda \
-    --yes --override-channels --channel conda-forge --strict-channel-priority \
-    pip  python=3.12 conda-build conda-forge-ci-setup=4 "conda-build>=24.1"
+#micromamba install --root-prefix ~/.conda --prefix /opt/conda \
+#    --yes --override-channels --channel conda-forge --strict-channel-priority \
+#    pip  python=3.12 conda-build conda-forge-ci-setup=4 "conda-build>=24.1"
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
 
 # set up the condarc
